dec149617031ed8742d043e7a114442f1ea17a47
Aditya Sharma <114848909+Aditya1or0@users.noreply.github.com>
2024-10-13 20:53:25 +0530

    Update and rename squarePattern.java to SquarePattern.java

--- Pattern/SquarePattern.java
+++ Pattern/SquarePattern.java
@@ -1,7 +1,7 @@
 package Pattern;
 
 
-public class squarePattern {
+public class SquarePattern {
     public static void main(String[] args) {
         for(int i=1;i<=4;i++){
             for(int j=1;j<=5;j++){
