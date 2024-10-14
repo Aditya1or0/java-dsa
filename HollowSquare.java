e218e5fcaf4a0e942d163071cefbcdef0a782ae3
Aditya Sharma <114848909+Aditya1or0@users.noreply.github.com>
2024-10-14 20:50:17 +0530

    Update and rename Pattern/pattern2.java to HollowSquare.java

--- HollowSquare.java
+++ HollowSquare.java
@@ -1,6 +1,6 @@
 package Pattern;
 
-public class pattern2 {
+public class HollowSquare {
     public static void main(String[] args) {
         for(int i=1;i<=4;i++){
             for(int j=1; j<=5 ;j++){
