c23bd333b49026ea4353f8ea34155f4b94bafdf6
Aditya Sharma <adityapandit264@gmail.com>
2024-11-03 13:36:21 +0530

    Update and rename pattern7.java to NumberIncreasingReversePyramid.java

--- Pattern/NumberIncreasingReversePyramid.java
+++ Pattern/NumberIncreasingReversePyramid.java
@@ -1,6 +1,6 @@
 package Pattern;
 
-public class pattern7 {
+public class NumberIncreasingReversePyramid {
     public static void main(String[] args) {
         int n=5;
         for(int i=1;i<=n;i++){
